f577db63fadbbf8df5e29b8f26d076cc5192a115
junior-x <marcosmansur@outlook.com>
2021-08-08 17:57:21 -0300

    api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-<div align="center">
+<div >
   <h1>{{ titulo }}</h1>
 
   <ul>
     <li v-for="foto of fotos" >
-      <img :src="fotos.url" :alt="fotos.titulo">
+      <img :src="foto.url" :alt="foto.titulo">
     </li>
   </ul>  
 
@@ -12,21 +12,13 @@
 </template>
 
 <script>
+
 export default {
   
   data() {
     return {
       titulo: 'Pizza',
-      fotos: [
-        {
-          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
-          titulo: 'Cachorro'
-        },
-        {
-          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
-          titulo: 'Gato'
-        },
-      ]
+      fotos: []
 
     }
   }
