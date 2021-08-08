aabe005e3d367b7a5f94df50faa3905280cda86c
junior-x <marcosmansur@outlook.com>
2021-08-08 17:23:48 -0300

    v-for

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <div align="center">
   <h1>{{ titulo }}</h1>
 
-  <ul :id="fotos.id">
+  <ul>
     <li v-for="foto of fotos" >
       <img :src="fotos.url" :alt="fotos.titulo">
     </li>
@@ -16,20 +16,18 @@ export default {
   
   data() {
     return {
-
-      titulo: 'STOP-MOTION',
+      titulo: 'Pizza',
       fotos: [
         {
-          url: 'https://www.wheretraveler.com/sites/default/files/images/pizzad.jpg',
-          titulo: 'stopMotion',
-          id: 'foto1',
+          url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+          titulo: 'Cachorro'
         },
         {
-          url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gannett-cdn.com%2Fpresto%2F2020%2F03%2F02%2FPDTN%2Fd8f03277-3956-4d29-83a7-ee8a592290ed-PP_-_Deep_Dish_Detroit_Style.jpg%3Fcrop%3D2999%2C1687%2Cx0%2Cy277%26width%3D2999%26height%3D1687%26format%3Dpjpg%26auto%3Dwebp&f=1&nofb=1',
-          titulo: 'stopMotion',
-          id: 'foto2',
-        }
+          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
+          titulo: 'Gato'
+        },
       ]
+
     }
   }
 }
